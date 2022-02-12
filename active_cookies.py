433efb8697be12fd9f32500004d69daa3e7d5c53
SrijaRedd6 <65451996+SrijaRedd6@users.noreply.github.com>
2022-02-12 12:40:30 -0600

    Add files via upload

---
+++ active_cookies.py
@@ -0,0 +1,30 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Dsiplaying most active cookies
+Created on Sat Feb 12 11:18:37 2022
+@author: srija
+"""
+
+import sys
+import pandas as pd
+from datetime import date,datetime,time
+
+def usage(): #function to check arguments from Command Line Interface
+    print("Usage: {sys.argv[0]} <csv_file> -d \"yyyy-mm-dd\"")
+
+
+# Check command line arguments.
+if len(sys.argv) != 4 or sys.argv[2] != '-d':
+    usage()
+    exit(1)
+    
+req_datetime=sys.argv[3]#read required date.
+data=pd.read_csv(sys.argv[1],encoding='utf-8')#read csv file
+df=pd.DataFrame(data)
+df[["date","time"]]=df['timestamp'].str.split("T",expand=True) #Splitting timestamp column with date and time
+df.drop(df[df['date'] != req_datetime].index, inplace = True)  #Dropping rows that are not equal to required date
+t=df["cookie"].mode()#Get the most used cookies
+print(t.to_string(index=False))
+
+
